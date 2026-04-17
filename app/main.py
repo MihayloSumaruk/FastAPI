de33bdd9348895c1462061ac4476a70fc039d089
Mihaylo Sumaruk <misha30sumaruk@gmail.com>
2026-04-17 20:57:56 +0300

    Global save: Lab 3 full progress and reports

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
-app = FastAPI(title="Lab 1 API")
+from app.api.users import router as users_router
+
+app = FastAPI(title="Lab 3 - FastAPI CRUD")
+
+app.include_router(users_router)
+
 @app.get("/")
-def read_root(): return {"message": "Lab 1 Template"}
+def root():
+    return {"message": "API is working. Go to /docs for Swagger UI"}
